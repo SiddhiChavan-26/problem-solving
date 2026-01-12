f6fc53000a7ee179f7973a0b2fcb7d56e180a7c9
Siddhi Chavan <siddhichavan073@gmail.com>
2026-01-12 21:23:09 +0530

    Add files via upload

---
+++ InvertedStar.java
@@ -0,0 +1,11 @@
+class InvertedStar{
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= (n-i+1); j++){
+                System.err.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
